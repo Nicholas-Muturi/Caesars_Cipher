9075cf0f23fb45977c1983619feba4f771a85053
Nicholas M <nicholasmuturi1@gmail.com>
2019-02-26 22:41:14 +0300

    Test to get the user key for encryption passes

--- src/main/java/CaesarCipher.java
+++ src/main/java/CaesarCipher.java
@@ -1,3 +1,22 @@
 public class CaesarCipher {
-  
+  private static char[] alphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+  private char[] userPlainInput;
+  private int userKey;
+  private char[] userEncrypted;
+  private char[] userDecrypted;
+
+  public CaesarCipher(char[] userInput, int userNumber){
+      userPlainInput = userInput;
+      userKey = userNumber;
+      userEncrypted = new char[userPlainInput.length];
+      userDecrypted = new char[userPlainInput.length];
+    }
+
+  public char[] getUserPlain(){
+    return userPlainInput;
+  }
+
+  public int getUserKey(){
+    return userKey;
+  }
 }
--- src/test/java/cipherTest.java
+++ src/test/java/cipherTest.java
@@ -2,5 +2,10 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class cipherTest {
-  
+
+  @Test
+  public void runCipher_getUserInput_true(){
+    CaesarCipher cipher = new CaesarCipher("this is a string".toCharArray(), 3);
+    assertEquals(3, cipher.getUserKey());
+  }
 }
