9f9be88d5a9c1cacd9ac599e6ba153323f0bbb94
Nicholas M <nicholasmuturi1@gmail.com>
2019-02-26 22:42:55 +0300

    Added front-end code to app.java to capture user input

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,14 @@ import java.util.*;
 
 public class App{
   public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
 
+    System.out.println("Enter your sentence");
+    char[] userSentence = input.nextLine().toLowerCase().toCharArray();
+    System.out.println("Enter the number of letters to shift by");
+    int userShift = input.nextInt();
+
+    char[] userEncrypted = new char[userSentence.length];
+    CaesarCipher cipher = new CaesarCipher(userSentence,userShift);
   }
 }
